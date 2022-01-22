f27eedf2128b28b9829ce97a3058e75a4e0a4ba6
Davide <davide@ueppy.com>
2022-01-22 20:53:27 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IonVue 6 Starter',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
